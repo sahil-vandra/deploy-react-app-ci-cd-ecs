0999bfc2106e842c0b76229c1f0d1e61891080e9
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-06-03 12:37:55 +0530

    3jun 11:45 PM

--- script-prod.sh
+++ script-prod.sh
@@ -73,6 +73,6 @@ echo "------------------------------------------ updare new service ------------
 aws ecs update-service --region ap-south-1 --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}" --force-new-deployment 
 echo "----------------------------------------- new service updated -----------------------------------------"
 
-aws ecs describe-services --services "${SERVICE_NAME}"
+aws ecs describe-services --cluster "${CLUSTER_NAME}" --services "${SERVICE_NAME}"
 
 # SERVICE_TASK_STATUS=`aws ecs describe-services --services "${SERVICE_NAME}" | jq .taskDefinition.containerDefinitions[].name`
